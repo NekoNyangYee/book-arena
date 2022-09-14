79d92531cf59fb0647380caeea57251f725df07e
NekoNyangYee <78199616+NekoNyangYee@users.noreply.github.com>
2022-09-14 20:02:42 +0900

    404 error img
    minor update at 2022/09/14

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,9 @@
-
+import './Css/Home.css';
 
 export default function Home () {
     return (
         <div>
-            <h1>홈 화면은 현재 개발중에 있습니다.<br /> 정식버전을 기다려주세요 감사합니다 :)</h1>
+            <div className= 'error-img'><img src='./img/404-Error.png' /></div>
         </div>
     );
 }
